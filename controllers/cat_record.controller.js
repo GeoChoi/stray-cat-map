75b3f29f4bebbc69d177b7eabc14613705e8d2fe
miiniipark <miiniipark@gmail.com>
2022-11-04 21:18:13 +0900

    feat: cat-records GET 메소드 수정 (거리, 위도, 경도, 시간)
    쿼리스트링에 포함된 파라미터를 검색 조건에 추가함.
    포함된 파라미터가 없을 경우 전체 리스트를 반환함.

--- controllers/cat_record.controller.js
+++ controllers/cat_record.controller.js
@@ -1,15 +1,28 @@
 const { Sequelize, cat_record: catRecord } = require('../db/models');
 
-exports.findAll = async (req, res) => {
-  const latitude = req.query.latitude;
-  const longitude = req.query.longitude;
-
+const searchByDistance = (distance = 1000, latitude = 37.5666805, longitude = 126.9784147) => {
   const here = Sequelize.fn('Point', longitude, latitude);
   const there = Sequelize.fn('Point', Sequelize.col('longitude'), Sequelize.col('latitude'));
-  const distance = Sequelize.fn('ST_Distance_Sphere', here, there);
+  const lhs = Sequelize.fn('ST_Distance_Sphere', here, there); // unit: meters
+  return Sequelize.where(lhs, { [Sequelize.Op.lte]: distance });
+};
 
-  catRecord.findAll({ attributes: [[distance, 'distance']] })
-    .then(result => {
-      res.send(result);
-    });
+const searchByHour = hour => {
+  const rhs = new Date(new Date() - hour * 60 * 60 * 1000); // unit: milliseconds
+  return { created_at: { [Sequelize.Op.gte]: rhs } };
+};
+
+const parseQuery = req => {
+  const conditions = [];
+  if (req.query.distance || req.query.latitude || req.query.longitude)
+    conditions.push(searchByDistance(req.query.distance, req.query.latitude, req.query.longitude));
+  if (req.query.hour)
+    conditions.push(searchByHour(req.query.hour));
+  return conditions;
+}
+
+exports.findAll = async (req, res, next) => {
+  catRecord.findAll({ where: { [Sequelize.Op.and]: parseQuery(req) } })
+    .then(result => res.json(result))
+    .catch(err => next(err));
 };
